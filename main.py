8f1b3f18ca55a1957a0f14b4bf696a0a8cdcd247
Artur <osiakowski.a@gmail.com>
2024-02-26 21:58:28 +0100

    Change home page heading

--- main.py
+++ main.py
@@ -44,6 +44,7 @@ class Base(DeclarativeBase):
 
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URL", "sqlite:///posts.db")
+# app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///posts.db"
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
